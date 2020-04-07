3a4ff2946629b4722312d9ed61f69d1b057db908
living4574 <adolfd@naver.com>
2020-04-07 18:42:33 +0900

    make argv and initial statet count function

---
+++ gameOfLife.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define endl "\n"
+using namespace std;
+
+int board[80][60];
+
+void makeState(int stateCount) {
+
+}
+
+int main(char **argv) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int arg1 = atoi(argv[1]);
+	int arg2 = atoi(argv[2]);
+
+
+}
